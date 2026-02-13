b22a0d20bf549aa71713a8321c76d8e44523c23c
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-13 17:58:39 +0000

    Update functions.sh
    Added source_bash

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,1 +1,10 @@
 # Bash functions
+
+source_bash()
+{
+	# Source setup shell scripts in same directory
+	for file in $(find $(dirname -- "$0") -type f -name "*.sh" ! -name $(basename "$0"));
+	do
+  		source $file;
+	done
+}
