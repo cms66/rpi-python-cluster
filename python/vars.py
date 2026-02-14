c69f9356ebfdf083220df2885a24fc76fd1cfc95
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-14 13:12:24 +0000

    Update vars.py

--- python/vars.py
+++ python/vars.py
@@ -4,3 +4,22 @@ import os
 hstname = str(os.uname()[1])
 usrname = os.getlogin() # Gives user with/without sudo
 arrconf = {}
+
+# Setup menus
+mnuMainFull = [
+"Setup - Main menu#",
+"Update setup#update_setup()",
+"Update system#update_system()",
+"Hardware#show_menu(pv.mnuHardwareFull)",
+"NFS#show_menu(pv.mnuNFSFull)",
+"ZFS#show_menu(pv.mnuZFSFull)",
+"Cluster#show_menu(pv.mnuClusterFull)", 
+"Modules#show_menu(pv.mnuModulesFull)",
+"OpenCV#show_menu(pv.mnuOpenCVFull)",
+"SDM#show_menu(pv.mnuSDMFull)",
+"System summary#run_bash('show_system_summary')",
+"SSH keys#show_menu(pv.mnuSSHFull)",
+"Check logs#check_logs",
+"Test#test_func",
+"Data management#show_menu(pv.mnuDataFull)",
+"Back|Quit#"]
