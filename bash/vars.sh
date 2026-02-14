9009bd8f360df3d2a6cf2db2e943c02ecdf995a6
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-14 15:31:04 +0000

    Update vars.sh

--- bash/vars.sh
+++ bash/vars.sh
@@ -1,2 +1,8 @@
 # Bash variables
-declare -A arrconf
+usrname=$(logname) # Script runs as root
+usrpath="/home/$usrname"
+declare -A arrconf=()
+pinum=$(hostname | tr -cd '[:digit:].')
+pimodel=$(cat /sys/firmware/devicetree/base/model | cut -d " " -f3- | tr -d "\0")
+pirev=$(cat /proc/cpuinfo | grep 'Revision' | awk '{print $3}' | sed 's/^1000//')
+osarch=$(getconf LONG_BIT)
