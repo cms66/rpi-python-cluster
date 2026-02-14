92940b4e4e2537ab6b7f04c85cf3fb54969d4fcc
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-14 16:46:13 +0000

    Update functions.py

--- python/functions.py
+++ python/functions.py
@@ -5,7 +5,7 @@ import python.vars as pv
 
 def set_owner(path):
 	id = pv.usrid
-	input("ID = " + id)
+	input("ID = " + str(id))
 	for root, dirs, files in os.walk(path):
 		# set perms on sub-directories  
 		for momo in dirs:
