b7aa8b2ea1616ed3ec1fb5ff1683f7b0b9f8ba4d
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-14 18:49:32 +0000

    Update vars.py

--- python/vars.py
+++ python/vars.py
@@ -3,8 +3,6 @@ import os
 
 hstname = str(os.uname()[1])
 usrname = os.getlogin() # Gives user with/without sudo
-#multipi:x:1000:1000
-usrid = os.system("'cat /etc/passwd | grep ' + usrname")
 arrconf = {}
 
 # Setup menus
