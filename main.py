de9ac51acdec97b472c71452f521aec17ee0eecd
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-14 13:27:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,8 @@ import python.functions as pf
 import python.vars as pv
 	
 def main():
-	input("Python main start")
 	pf.read_config()
 	pf.show_menu(pv.mnuMainFull)
-	#pf.run_bash('show_vars; test_func')
-	#pf.update_setup()
-	#print("Git dir = " + pv.arrconf['gitlocaldir'] + "\n")
-	input("Python main end")
 
 if __name__ == "__main__":
 	main()
