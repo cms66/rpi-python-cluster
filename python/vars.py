f58adce9b563b21787cfd54a3d53a91203534f31
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-14 14:46:49 +0000

    Update vars.py

--- python/vars.py
+++ python/vars.py
@@ -20,6 +20,6 @@ mnuMainFull = [
 "System summary#run_bash('show_system_summary')",
 "SSH keys#show_menu(pv.mnuSSHFull)",
 "Check logs#check_logs",
-"Test#test_func",
+"Test#run_bash('bash_test')",
 "Data management#show_menu(pv.mnuDataFull)",
 "Back|Quit#"]
