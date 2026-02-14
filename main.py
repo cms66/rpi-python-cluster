0d678f57f2936a9a821677cf6c28964a51cc8005
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-14 12:19:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 # Python main entry point for setup + management
-print("Python main start")
-input("Press enter to continue")
-print("Python main end")
+
+import os
+import python.functions as pf
+import python.vars as pv
+	
+def main():
+	input("Python main start")
+	#pf.run_bash('show_vars; test_func')
+  pf.update_setup()
+	input("Python main end")
+
+if __name__ == "__main__":
+	main()
