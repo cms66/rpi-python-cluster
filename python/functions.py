1440a70eed028e4c6f0a13a757a6256f5efd7368
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-14 12:57:44 +0000

    Update functions.py

--- python/functions.py
+++ python/functions.py
@@ -4,7 +4,7 @@ import os
 import python.vars as pv
 
 def run_bash(func):
-	bashfile = os.path.dirname(__file__) + "/bash/functions.sh"
+	bashfile = os.path.dirname(__file__) + "../bash/functions.sh"
 	print("File = " + bashfile)
 	#strcmd = "source " + bashfile + "; source_bash; " + func
 	#os.system(strcmd)
