fa4305a9bc2e9c7d0be4a12142da10e55ad77e79
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-14 14:58:24 +0000

    Update functions.sh

--- bash/functions.sh
+++ bash/functions.sh
@@ -9,7 +9,19 @@ source_bash()
 	done
 }
 
+read_config()
+{
+	input="/boot/firmware/custom.conf"
+	while IFS= read -r line
+	do
+		[ "${line:0:1}" = "#" ] || [ "${line:0:1}" = "" ] && continue # Ignore comment and empty lines works
+		key=$(echo $line | cut -d "=" -f1)
+		value=$(echo $line | cut -d "=" -f2)
+		arrconf+=([$key]="$value")
+	done < "$input"
+}
+
 bash_test()
 {
-	read -p "bash_test in functions.sh"
+	read -p "bash_test in functions.sh. Git dir = $arrconf['gitlocaldir']"
 }
