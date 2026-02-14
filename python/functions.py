a2a02cf3377f87c505af269b21f4e1ff533fa7b7
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-14 12:19:34 +0000

    Update functions.py

--- python/functions.py
+++ python/functions.py
@@ -1,2 +1,28 @@
 # Python functions
-#
+
+import os
+import python.vars as pv
+
+def run_bash(func):
+	strcmd = "source " + pv.bashfile + "; source_bash; " + func
+	os.system(strcmd)
+	input("Command done, press enter to continue")
+
+def read_config():
+	conf = "/boot/firmware/custom.conf"
+	if os.path.exists(conf):
+		with open(conf) as f:
+			for line in f:
+				key = line.split('=')[0]
+				val = line.split('=')[1]
+				sv.arrconf[key] = val
+
+def update_setup():
+	strdir = arrconf["gitlocaldir"] + "/" +  arrconf["gitrepo"].strip()
+	gitdir = "".join(strdir.splitlines())
+	strurl = "https://github.com/" +  arrconf["gituser"] + "/" +  arrconf["gitrepo"] + ".git".strip()
+	giturl = "".join(strurl.splitlines())
+	cmd = "git pull " + giturl + " " + gitdir
+	os.chdir(gitdir)
+	os.system(cmd)
+	input("Git setup done, press enter to continue")
