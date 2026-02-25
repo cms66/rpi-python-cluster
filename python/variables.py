282ae8c78327245c823069a4d895e09cbc6ecd44
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-25 18:42:57 +0000

    Create variables.py

---
+++ python/variables.py
@@ -0,0 +1,29 @@
+# Python variables
+import os
+
+hstname = str(os.uname()[1])
+usrname = os.getlogin() # Gives user with/without sudo
+# TODO get usrid from usrname 
+#usrid = os.getuid()
+#usrid = int(os.system("cat /etc/passwd | grep " + usrname + " cut -d ':' -f 3"))
+usrid = 1000
+arrconf = {}
+
+# Setup menus
+mnuMainFull = [
+"Setup - Main menu#",
+"Update setup#update_setup()",
+"Update system#update_system()",
+"Hardware#show_menu(pv.mnuHardwareFull)",
+"NFS#show_menu(pv.mnuNFSFull)",
+"ZFS#show_menu(pv.mnuZFSFull)",
+"Cluster#show_menu(pv.mnuClusterFull)", 
+"Modules#show_menu(pv.mnuModulesFull)",
+"OpenCV#show_menu(pv.mnuOpenCVFull)",
+"SDM#show_menu(pv.mnuSDMFull)",
+"System summary#run_bash('show_system_summary')",
+"SSH keys#show_menu(pv.mnuSSHFull)",
+"Check logs#check_logs",
+"Test#set_owner('/data/current/src/git', pv.usrname)",
+"Data management#show_menu(pv.mnuDataFull)",
+"Back|Quit#"]
