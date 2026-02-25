9e80c79f49d79157d6c796d6f97d234eb627856c
cms66 <58144229+cms66@users.noreply.github.com>
2026-02-25 17:12:34 +0000

    Update functions.py

--- python/functions.py
+++ python/functions.py
@@ -5,7 +5,6 @@ import python.vars as pv
 
 def set_owner(path, user):
 	id = pv.usrid
-	input("User = " + str(user))
 	input("ID = " + str(id))
 	input("Path = " + path)
 	for root, dirs, files in os.walk(path):
