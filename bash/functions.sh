6b9c0bc266c73dad8ffa06f597dc3178dbbbae02
cms66 <58144229+cms66@users.noreply.github.com>
2026-04-25 12:47:20 +0100

    Update functions.sh

--- bash/functions.sh
+++ bash/functions.sh
@@ -58,3 +58,95 @@ delete_node_from_known_hosts()
 	read -p "Remote node: " remnode
 	sudo -u $usrname ssh-keygen -f "$usrpath/.ssh/known_hosts" -R $remnode
 }
+
+# NFS setup functions
+#--------------------
+install_nfs_server()
+{
+	# TODO - Firewall rule not added if called from add_nfs_local
+	# Messages from install_nfs_server option are
+	# 	nfs-kernel-server install done, press enter to continue
+	# 	Device = eth0 | localnet = 192.168.0.0/24
+	# 	Rule added
+
+ 	#statnfs=$(check_package_status nfs-kernel-server y) # Check installed + install if not
+	statnfs="d"
+	if [[ $statnfs = "d" ]]; then # Setup server
+	 	# Enforce NFSv4
+		echo "RPCMOUNTDOPTS=\"--manage-gids -N 2 -N 3\"" >> /etc/default/nfs-kernel-server
+		echo "RPCNFSDOPTS=\"-N 2 -N 3\"" >> /etc/default/nfs-kernel-server
+    	get_subnet_cidr
+    	yes | sudo ufw allow from $localnet to any port nfs
+		read -p "NFS Server setup done, press any key to continue"
+	else
+		read -p "NFS Server already installed, press any key to continue"
+	fi
+}
+
+# Add local export
+add_nfs_local()
+{
+	get_subnet_cidr
+	install_nfs_server
+   	# Check mount type
+    read -p "System mount (default $defsysdir) or Data share? (s/d) " inp
+    if [[ ${inp,} = "s" ]]; then # System mount
+    	read -p "Path of directory to be shared (press enter for default = $defsysdir): " userdir
+		nfsdir=${userdir:="$defsysdir"}
+		# Check mount exists
+		if grep -F "$nfsdir" "/etc/exports"
+		then
+			read -p "export  exists"
+		else
+			echo "$nfsdir $localnet(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
+			exportfs -ra
+			read -p "export $nfsdir created"
+		fi
+	elif [[ ${inp,} = "d" ]]
+	then # Data mount (default /data/subdirectory)    	
+		read -p "Path to directory for export (press enter for default = $defdatadir): " userdir
+		basedir=${userdir:="$defdatadir"}
+ 		# Data share path/name (default /data/share name)
+   		nfsbase=$(list_selection "directory" d $basedir)
+		nfsdir=$nfsbase
+		# Check mount exists
+		if grep -F "$nfsdir" "/etc/exports"
+		then
+			read -p "export  exists"
+		else
+			echo "$nfsdir $localnet(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
+			exportfs -ra
+			read -p "export $nfsdir created"
+			create_data_structure $nfsdir
+		fi
+		mount -a
+ 		#read -p "NFS export added, press any key to return to menu" input
+    else
+    	read -p "invalid input"
+    fi
+}
+
+# Add remote mount
+add_nfs_remote()
+{
+	read -p "Remote node: " remnode
+	# Check mount type
+    	read -p "System mount (default $defsysdir) or Data share? (s/d): " inp
+    	if [[ ${inp,} = "s" ]]
+    	then # System mount
+     		read -p "Full path to remote directory (press enter for default = $defsysdir): " userdir
+       		mntdir=${userdir:="$defsysdir"}
+	 	echo "$remnode:$mntdir $mntdir    nfs4 rw,relatime,rsize=32768,wsize=32768,namlen=255,hard,proto=tcp,timeo=600,retrans=2,sec=sys,local_lock=none 0 0" >> /etc/fstab
+   		ldconfig
+	elif [[ ${inp,} = "d" ]]
+	then # Data mount (default /data/subdirectory)
+		read -p "Path to remote directory (press enter for default = $defdatadir): " userdir
+		basedir=${userdir:="$defdatadir"}
+ 		# Data share path/name (default /data/share name)
+   		mntdir=$(list_selection "directory" d $basedir)
+		echo "$remnode:$mntdir $mntdir    nfs4 rw,relatime,rsize=32768,wsize=32768,namlen=255,hard,proto=tcp,timeo=600,retrans=2,sec=sys,local_lock=none 0 0" >> /etc/fstab
+		systemctl daemon-reload  		
+	fi
+	mount -a
+	read -p "NFS remote mount done, press enter to return to menu"
+}
